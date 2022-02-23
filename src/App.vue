183f8b2298ae59fe1893a92d4efa30e56a604685
yuwen <yuwennweng@gmail.com>
2022-02-23 15:57:08 +0800

    resetCSS

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,4 +27,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
